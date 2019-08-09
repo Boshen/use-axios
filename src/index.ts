b5a44c7fbe6319871a5424b068304170ec8c3c51
Boshen <boshenc@gmail.com>
2019-08-09 22:59:15 +1200

    fix: immediately reset state after dependencies have changed

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 import axios, { AxiosRequestConfig, AxiosError } from 'axios'
 
-interface UseAxiosState<T> {
-  data: T | undefined
-  loading: boolean
-  error: AxiosError<T> | undefined
-}
-
 export interface UseAxiosOptions {
   skipRequest?: () => boolean
 }
 
 export type UseAxiosConfig = AxiosRequestConfig & UseAxiosOptions
+
+interface UseAxiosState<T> {
+  data: T | undefined
+  loading: boolean
+  error: AxiosError<T> | undefined
+}
 
 const toState = (loading: boolean, data?: any, error?: AxiosError) => ({
   data,
@@ -20,12 +20,21 @@ const toState = (loading: boolean, data?: any, error?: AxiosError) => ({
 })
 
 const useAxios = <T>(config: UseAxiosConfig, dependencies: any[]) => {
-  const useStateReturn = useState<UseAxiosState<T>>(toState(true))
+  const skipRequest = config.skipRequest || (() => false)
+  const useStateReturn = useState<UseAxiosState<T>>(toState(!skipRequest()))
   const state = useStateReturn[0]
   const setState = useStateReturn[1]
+  const prevDepsReturn = useState(dependencies)
+  const prevDeps = prevDepsReturn[0]
+  const setPrevDeps = prevDepsReturn[1]
+
+  if (!areHookInputsEqual(dependencies, prevDeps)) {
+    setState(toState(!skipRequest()))
+    setPrevDeps(dependencies)
+  }
 
   useEffect(() => {
-    if (config.skipRequest && config.skipRequest()) {
+    if (skipRequest()) {
       return
     }
 
@@ -49,6 +58,16 @@ const useAxios = <T>(config: UseAxiosConfig, dependencies: any[]) => {
   }, dependencies)
 
   return state
+}
+
+function areHookInputsEqual(nextDeps: any[], prevDeps: any[]) {
+  for (let i = 0; i < prevDeps.length && i < nextDeps.length; i++) {
+    if (Object.is(nextDeps[i], prevDeps[i])) {
+      continue
+    }
+    return false
+  }
+  return true
 }
 
 export default useAxios
